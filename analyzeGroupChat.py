3b8bc8e08a7e976b1e8bee878c0d53c6842d850f
cathepsin.z <zachercaleb@gmail.com>
2021-07-05 15:51:52 +0800

    Added stats for messages and images sent by participants

--- analyzeGroupChat.py
+++ analyzeGroupChat.py
@@ -16,14 +16,42 @@ def main():
     for name in chat['participants']:
         participants.append(name['name'])
     
+    #This tracks how many messages each participant has sent
+    messagesSent = dict.fromkeys(participants, 0)
+    #This tracks how many images each participant has sent
+    imagesSent = dict.fromkeys(participants, 0)
     #This tracks how many reactions each participant made
     reactionsMade = dict.fromkeys(participants, 0)
     #This tracks how many reactions each participant received
     reactionsReceived = dict.fromkeys(participants, 0)
 
+    calcMessagesSent(chat, messagesSent)
+    calcImagesSent(chat, imagesSent)
     calcReactions(chat, reactionsMade, reactionsReceived)
-    print(reactionsMade)
-    print(reactionsReceived)
+
+    #Print the results
+    print('Messages sent:\n', messagesSent)
+    print('Images sent:\n', imagesSent)
+    print('Reactions:\n', reactionsMade)
+    print('Reactions received:\n', reactionsReceived)
+
+#Counts the total number of messages sent by each participant
+def calcMessagesSent(chat, messagesSent):
+    #For every message
+    for message in chat['messages']:
+        #Update the message count for the sender
+        messagesSent[message['sender_name']] = messagesSent[message['sender_name']] + 1
+
+#Counts the total number of images sent by each participant
+def calcImagesSent(chat, imagesSent):
+    #For every message
+    for message in chat['messages']:
+        #If there is an image in the message
+        if 'photos' in message:
+            #Count the number of photos sent
+            numImagesSent = len(message['photos'])
+            #Update the image count for the sender
+            imagesSent[message['sender_name']] = imagesSent[message['sender_name']] + numImagesSent   
 
 #Counts the total number of reactions each participant made
 def calcReactions(chat, reactionsMade, reactionsReceived):
